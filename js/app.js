2644ef8bb5258538744c95ad934a4ecfa9fadb47
ricardomlourenco <ricardomlourenco@gmail.com>
2022-07-29 20:46:32 -0400

    Initial project...

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// MODULE
+var app = angular.module('app', []);
+
+// CONTROLLERS
+app.controller('mainController', ['$scope', '$filter', function ($scope, $filter) {
+
+    $scope.title = 'AngularJS minimal...';
+    
+}]);
+
+
+
+
+
+
